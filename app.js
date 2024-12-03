eae3e068debd08393dcea8c90af77c53dd306acc
Karnjyot <krnjyotsingh2004@email.com>
2024-12-03 09:09:06 +0100

    fine esercizio

---
+++ app.js
@@ -0,0 +1,8 @@
+const name = process.argv
+console.log(name)
+//Parola passata come argomento
+const userName = process.argv[2]
+console.log(userName);
+console.log(`Hello ${userName}`)
+//Stampa Hello Boolean
+console.log("Hello Boolean")
